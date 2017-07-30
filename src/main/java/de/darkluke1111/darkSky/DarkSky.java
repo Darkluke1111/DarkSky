1f933c4a5e45d04da9468a0e80f11a2974c2b715
Darkluke1111 <lukas.gromer@freenet.de>
2017-07-30 17:46:19 +0200

    Initial Commit

---
+++ src/main/java/de/darkluke1111/darkSky/DarkSky.java
@@ -0,0 +1,11 @@
+package de.darkluke1111.darkSky;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DarkSky extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("de.darkluke1111.darkSky.DarkSky says hi!");
+    }
+}
